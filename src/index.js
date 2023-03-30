73e052135cbe83b6545a59efe94f07dcdd1ca25f
ketherball <121590283+ketherball@users.noreply.github.com>
2023-03-30 15:53:39 -0400

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './styles.css'
+
+const body = document.querySelector('body')
+const div = document.createElement('div')
+
+div.textContent = 'Hello World'
+div.classList.add('styles')
+
+body.appendChild(div)
